20bf316348c47f1609449e94652267ee0267d653
HungHH <hunghh.dev@gmail.com>
2019-09-02 00:10:05 +0700

    [Client] Fix ip client

--- hotelmanagement/config/index.js
+++ hotelmanagement/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: "127.0.0.1", // can be overwritten by process.env.HOST
-    port: 10000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     service: "http://127.0.0.1:9999",
     autoOpenBrowser: false,
     errorOverlay: true,
