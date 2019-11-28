c94e1ea3d072252212afaf88f8bc51d69ef24ff2
HungHH.Dev <hunghh.dev@gmail.com>
2019-11-28 22:06:11 +0700

    fix customerDto

--- WebService/src/main/java/com/aptech/project/hotel/model/CustomerDto.java
+++ WebService/src/main/java/com/aptech/project/hotel/model/CustomerDto.java
@@ -1,12 +1,16 @@
 package com.aptech.project.hotel.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.annotation.Id;
 
 @Getter@Setter
 @Document(indexName = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
 
     @Id
@@ -15,13 +19,4 @@ public class CustomerDto {
     private String phone;
     private String identification;
 
-    public CustomerDto(int id, String name, String phone, String identification) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.identification = identification;
-    }
-
-    public CustomerDto() {
-    }
 }
